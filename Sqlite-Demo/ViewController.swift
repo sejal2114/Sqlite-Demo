62f012a36e94330f54bb2e0cafe5ff143b255a56
sejal <sejal4789@gmail.com>
2023-03-01 12:01:38 +0530

    Sqlite demo

--- Sqlite-Demo/ViewController.swift
+++ Sqlite-Demo/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var db:DBhelper = DBhelper()
+        
+     //   var persons:[Employee] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      db.insert(id: 1, name: "sejal", age: 27)
+    db.insert(id: 2, name: "akshay", age: 29)
+        db.insert(id: 3, name: "Eva", age: 4)
+        db.insert(id: 4, name: "sjs", age: 5)
+        db.read()
+        
+      // let empolyees = db.read()
+      
     }
 
+  
 
 }
 
